f5c8a17a1df8a959e9a7081ce20936879a35bb50
MatthewFro191919 <matthewfroncek4@gmail.com>
2024-12-11 19:10:38 -0500

    Update Boyfriend3D.hx

--- source/Boyfriend3D.hx
+++ source/Boyfriend3D.hx
@@ -4,6 +4,7 @@ import flixel.util.FlxDestroyUtil;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.graphics.frames.FlxAtlasFrames;
+import funkin.game.Character3D;
 
 using StringTools;
 
